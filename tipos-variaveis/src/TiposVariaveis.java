ff11acaa1e3bb8757c3101c156256ba15cc82b36
Gabriel Sales <gabrielhub16@gmail.com>
2023-01-24 14:41:25 -0300

    Code Java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class  TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+
+        double salarioMinimo =2500; 
+ 
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;//um inteior pode armazenar um short
+        short numeroCurto2 = (short) numeroNormal;//um short não pode armazenar um inteiro através do casting
+    
+        int numero =10;
+        System.out.println(numero);
+
+        final double VALOR_DE_PI=3.14;
+
     }
 }
